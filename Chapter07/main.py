0f08f44b5515956888a4a17eb2cf923fd2f822ac
Noguchi-Masashi <noguchi.kdk.mm@gmail.com>
2022-11-10 13:45:33 +0000

    conduct black and fix mnist data path

--- Chapter07/main.py
+++ Chapter07/main.py
@@ -12,25 +12,42 @@ from torch.distributed.optim import DistributedOptimizer
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data.distributed import DistributedSampler as DDP_sampler
 
-train_all_set = datasets.MNIST('./mnist_data', download=True, train=True,
-               transform = transforms.Compose([transforms.ToTensor(),
-               transforms.Normalize((0.1307,),(0.3081,))]))
-train_set, val_set = torch.utils.data.random_split( train_all_set,
-        			 [50000, 10000])
+train_all_set = datasets.MNIST(
+    '../mnist_data',
+    download=True,
+    train=True,
+    transform=transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+    ),
+)
+train_set, val_set = torch.utils.data.random_split(train_all_set, [50000, 10000])
 
-test_set = datasets.MNIST('./mnist_data', download=True, train=False,
-              transform = transforms.Compose([transforms.ToTensor(), 
-              transforms.Normalize((0.1307,),(0.3081,))]))
+test_set = datasets.MNIST(
+    '../mnist_data',
+    download=True,
+    train=False,
+    transform=transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+    ),
+)
+
 
 def train(args):
     model = MyNet()
     model.train()
-    trainset= datasets.MNIST('./mnist_data', download=True, train=True,
-               transform = transforms.Compose([transforms.ToTensor(),
-               transforms.Normalize((0.1307,),(0.3081,))]))
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=4)
-    criterion = nn.CrossEntropyLoss()			
-    optimizer = torch.optim.SGD(model.parameters(), lr = 1e-3)
+    trainset = datasets.MNIST(
+        '../mnist_data',
+        download=True,
+        train=True,
+        transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        ),
+    )
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=128, shuffle=True, num_workers=4
+    )
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
     for epoch in range(args.epochs):
         print(f"Epoch {epoch}")
@@ -45,12 +62,21 @@ def train(args):
             print(f"batch {idx} training :: loss {loss.item()}")
         print("Training Done!")
     return model
+
+
 def test(args, model):
     model.eval()
-    testset = datasets.MNIST('./mnist_data', download=True, train=True,
-               transform = transforms.Compose([transforms.ToTensor(),
-               transforms.Normalize((0.1307,),(0.3081,))]))
-    testloader = torch.utils.data.DataLoader(testset, batch_size=128,shuffle=False, num_workers=4)
+    testset = datasets.MNIST(
+        '../mnist_data',
+        download=True,
+        train=True,
+        transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        ),
+    )
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=128, shuffle=False, num_workers=4
+    )
     correct_total = 0
     with torch.no_grad():
         for idx, (data, target) in enumerate(testloader):
@@ -59,16 +85,18 @@ def test(args, model):
             target = target.to(output.device)
             correct = predict.eq(target.view_as(predict)).sum().item()
             correct_total += correct
-            acc = correct_total/len(testloader.dataset)
+            acc = correct_total / len(testloader.dataset)
             print(f"Test Accuracy {acc}")
     print("Test Done!")
 
+
 def main():
-    parser = argparse.ArgumentParser(description = 'model parallel training')
-    parser.add_argument('-e', '--epochs', default = 4, type = int, help='number of epochs')
+    parser = argparse.ArgumentParser(description='model parallel training')
+    parser.add_argument('-e', '--epochs', default=4, type=int, help='number of epochs')
     args = parser.parse_args()
     trained_model = train(args)
     test(args, trained_model)
+
 
 if __name__ == '__main__':
     main()
